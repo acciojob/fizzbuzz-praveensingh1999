8e6627fb0ce5cbf5552bf55955285e0f2905a29d
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-05-06 11:33:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let start = 1;
+let end=20;
+for(let i=start;i<=end;i++){
+	if(i%3==0 && i%5==0){
+		console.log("FizzBuzz");
+	}
+	else if(i%5==0){
+		console.log("Buzz");
+	}
+	else if(i%3==0){
+		console.log("Fizz");
+	}
+	else{
+		console.log(i.toString());
+	}
+}
